81f54df1db7358791410bb82e6010ff9f5d4eb11
Denis Karakoluichka <karakoluchka@kassir.ru>
2018-08-02 02:40:57 +0300

    fix npm

--- blog/resources/views/posts/_post.blade.php
+++ blog/resources/views/posts/_post.blade.php
@@ -9,15 +9,15 @@
                 </h2>
                 <div class="row">
                         <div class="col-sm-12">
-                            <i class="fa fa-clock-o"></i>
+                            <i class="fas fa-clock"></i>
                             <span class="text-muted">Published:</span>
                             <small class="text-muted">({{ date('d.m.Y', $post->ts) }})</small>
                         </div>
                     <div class="post-annotation col-sm-12">{{ strip_tags(substr($post->content,0, 40)) }}</div>
                         <div>
                             @if($post->canDelete())
-                                <a href="{{ route('post-delete', ['slug' => $post->slug]) }}" class="post-delete">Delete post</a>
-                                <a href="{{ route('post-create', ['postId' => $post->id]) }}" class="post-delete">Edit post</a>
+                                <a href="{{ route('post-delete', ['slug' => $post->slug]) }}" class="post-delete" title="Remove post"><i class="fas fa-trash-alt"></i></a>
+                                <a href="{{ route('post-create', ['postId' => $post->id]) }}" class="post-delete" title="Edit post"><i class="fas fa-edit"></i></a>
                                 @endif
                         </div>
                     </div>
