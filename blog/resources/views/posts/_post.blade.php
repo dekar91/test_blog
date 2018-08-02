962ef0b4a789236b7d4b35bedd3adbd576b59248
Denis Karakoluichka <karakoluchka@kassir.ru>
2018-08-02 03:03:33 +0300

    fix icons

--- blog/resources/views/posts/_post.blade.php
+++ blog/resources/views/posts/_post.blade.php
@@ -9,15 +9,15 @@
                 </h2>
                 <div class="row">
                         <div class="col-sm-12">
-                            <i class="fas fa-clock"></i>
+                            <i class="fa fa-clock-o"></i>
                             <span class="text-muted">Published:</span>
                             <small class="text-muted">({{ date('d.m.Y', $post->ts) }})</small>
                         </div>
                     <div class="post-annotation col-sm-12">{{ strip_tags(substr($post->content,0, 40)) }}</div>
-                        <div>
+                        <div class="post-buttons col-sm-12">
                             @if($post->canDelete())
-                                <a href="{{ route('post-delete', ['slug' => $post->slug]) }}" class="post-delete" title="Remove post"><i class="fas fa-trash-alt"></i></a>
-                                <a href="{{ route('post-create', ['postId' => $post->id]) }}" class="post-delete" title="Edit post"><i class="fas fa-edit"></i></a>
+                                <a href="{{ route('post-delete', ['slug' => $post->slug]) }}" class="post-delete" title="Remove post"><i class="fa fa-trash-o"></i></a>
+                                <a href="{{ route('post-create', ['postId' => $post->id]) }}" class="post-delete" title="Edit post"><i class="fa fa-edit"></i></a>
                                 @endif
                         </div>
                     </div>
